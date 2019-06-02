b87c05225bad53f36009ade573513556f7105ff7
Siddharth Gollapudi <siddharthgollapudi99@gmail.com>
2019-06-01 21:23:35 -0700

    Add project structure file and basic header files

--- src/commit_node.hpp
+++ src/commit_node.hpp
@@ -1,9 +1,42 @@
+/*
+ * commit_node.hpp
+ *
+ * created: May 30, 2019
+ * author: Siddharth Gollapudi
+ *
+ */
+
 #ifndef __COMMIT_NODE_H_
 #define __COMMIT_NODE_H_
 
+#include <string>
+#include <vector>
+
 class Commit{
-    public:
     private:
+
+        std::string hash, author, timestamp, message;
+        //std::vector<Blob> staged_files;
+
+        template <typename T>
+        std::string generate_hash(T input);
+
+    public:
+
+        //Commit(std::vector<Blob> v);
+
+        bool get_repo_tree();
+
+        std::string get_hash() { return hash; }
+        std::string get_author() { return author; }
+        std::string get_timestamp() { return timestamp; }
+        std::string get_message() { return message; }
+
+        void set_hash(std::string s) { hash = s; }
+        void set_author(std::string a) { author = a; }
+        void set_timestamp(std::string t) { timestamp = t; }
+        void set_message(std::string m) { message = m; }
+
 };
 
 #endif // __COMMIT_NODE_H_
--- src/main_tree.hpp
+++ src/main_tree.hpp
@@ -1,22 +1,46 @@
+/*
+ * main_tree.hpp
+ *
+ * Created: May 30, 2019
+ * Author: Siddharth Gollapudi
+ *
+ */
+
 #ifndef __MAIN_TREE_H_
 #define __MAIN_TREE_H_
 
 #include <string>
+#include <memory>
 #include "commit_node.hpp"
 
 /*
  * This tree should contain the entire commit chain of a git repository.
   */
-class Tree{
+class Tree {
     private:
-        struct Node{
-            std::string hash;
+
+        struct Node {
+            std::unique_ptr<Node> children;
             Commit c;
+
+            Node (Commit input) {
+                c = input;
+                children = NULL;
+            }
+
         };
+
+        Node HEAD;
+        std::string project_name;
 
 
     public:
-        Node initial;
+
+        Node get_HEAD();
+        void set_HEAD(Node n);
+
+        Node find_node(std::string hash);
+
 };
 
 #endif // __MAIN_TREE_H_
